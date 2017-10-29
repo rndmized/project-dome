7149c03a0408bc255382e368ba1ca4786e15e334
Unknown <a.rndmized@gmail.com>
2017-10-29 15:27:43 +0000

    Added Player Controller Script.
    Added script to move character. It requires the game object to contain a Character Controller component.

---
+++ Prototype/ProjectDomePrototype/Assets/Scripts/CharacterScripts/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Player Movement Settings
+    public float speed = 3f;
+    public float turnSpeed = 10f;
+    public float jumpDist = 3.0f;
+    public float gravity = 20f;
+
+
+    private Vector2 input;
+    private float angle;
+    private float verticalVelocity;
+
+    Quaternion targetRotation;
+    
+    //References to Main Camera, player transform and CharacterController component.
+    Transform cam, character;
+    CharacterController player;
+
+
+    //Initialize components
+    void Start()
+    {
+        cam = Camera.main.transform;
+        character = GetComponent<Transform>();
+        player = character.GetComponent<CharacterController>();
+    }
+
+
+
+    void Update()
+    {
+
+            GetInput();
+            ApplyGravity();
+            //Claculate direction to face to.
+            CalculateDirection();
+            //Rotate transform
+            Rotate();
+            //Move in that direction
+            Move();    
+    }
+
+
+    /* This function detects whether the player transfor is airborne and applies velocity downwards
+        to mimic gravity (sort of). Get input to jump only if the player is grounded.
+     */
+    private void ApplyGravity()
+    {
+
+        if (player.isGrounded == true)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = jumpDist;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+    }
+
+
+
+    /* Get input */
+    void GetInput()
+    {
+            input.x = Input.GetAxis("Horizontal");
+            input.y = Input.GetAxis("Vertical");
+
+    }
+
+    /* Calculate rotation angle using camera direction */
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+
+    }
+
+    /* Use angle and quaternion to rotate transform. Slerp to smooth transition. */
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+
+    /* Move forward in the direction the transform is facing. Use character controller component to move the transform.  */
+    private void Move()
+    {
+        Vector3 movement = new Vector3(0, verticalVelocity * Time.deltaTime, 0);
+        if (!(Mathf.Abs(input.x) < 0.1 && Mathf.Abs(input.y) < 0.1))
+        {
+            movement += transform.forward * speed * Time.deltaTime;
+        }
+        player.Move(movement);
+    }
+
+}
