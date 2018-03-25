736f3d2afdff02977f498d50831e714a3d7caa68
PedroHOMota <phomota@hotmail.com>
2018-03-25 20:53:25 +0100

    Started changing server to keep user info

---
+++ Servers/TCPIPServer/TCPServer/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerEcho
+{
+	public class Player
+	{
+		public string uName { get; set; } //User name
+		public string cName { get; set; } //Char name
+		public float cX { get; set; }
+		public float cY { get; set; }
+		public float cZ { get; set; }
+	}
+}
