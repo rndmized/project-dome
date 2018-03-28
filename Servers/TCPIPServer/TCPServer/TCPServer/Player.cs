4181dabb0dc9a0a259d9a92f5839bcc433fb184c
PedroHOMota <phomota@hotmail.com>
2018-03-28 11:25:29 +0100

    Started implementing connection to mongodb

---
+++ Servers/TCPIPServer/TCPServer/TCPServer/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerEcho
+{
+	public class Player
+	{
+		public string uName { get; set; } //User name
+		public string cName { get; set; } //Char name
+		public float cX { get; set; }
+		public float cY { get; set; }
+		public float cZ { get; set; }
+
+		public int head { get; set; }
+		public int body { get; set; }
+		public int cloths { get; set; }
+	}
+}
